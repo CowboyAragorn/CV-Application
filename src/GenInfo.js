a8621e6867392618ffffc0483dc6e53439d770d5
CowboyAragorn <jacobman0313@gmail.com>
2022-11-01 20:45:27 -0400

    first commit

---
+++ src/GenInfo.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+//A section to add general information like name, email, phone number.
+
+class GenInfo extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Hello World</p>
+      </div>
+    );
+  }
+}
+
+export default GenInfo;
