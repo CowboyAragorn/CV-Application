8a3162cf28d2fcd2efede55874c98d9d7ee4f1dd
CowboyAragorn <jacobman0313@gmail.com>
2022-11-04 12:02:55 -0400

    feat: jobExperience inputs are dynamically generated and deleted with state intact for each

--- src/components/JobExperience.js
+++ src/components/JobExperience.js
@@ -1,163 +1,103 @@
 import React, { Component } from "react";
 
-//A section to add practical experience
-//(company name, position title, main tasks of your jobs,
-//date from and until when you worked for that company)
+// /A section to add your educational experience
+//(school name, title of study, date of study)
+
+//transfer input values to display component
 class JobExperience extends Component {
   constructor() {
     super();
     this.state = {
-      jobExp: [],
-      companyName: " ",
-      mainTasks: " ",
-      startDate: " ",
-      endDate: "",
-      displayInputs: true,
-      editing: false,
-      key: 0,
-      editKey: 0,
+      values: [],
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.addNewJob = this.addNewJob.bind(this);
-    this.editJobOnClick = this.editJobOnClick.bind(this);
-    this.updateJob = this.updateJob.bind(this);
-    this.addAnotherJob = this.addAnotherJob.bind(this);
+
+    //this.handleChange = this.handleChange.bind(this);
+    this.addNewInputs = this.addNewInputs.bind(this);
+    this.removeExp = this.removeExp.bind(this);
   }
 
-  addNewJob() {
-    //check if it is an edit or a new item
-    this.setState(
-      {
-        jobExp: [
-          ...this.state.jobExp,
-          {
-            companyName: this.state.companyName,
-            mainTasks: this.state.mainTasks,
-            startDate: this.state.startDate,
-            endDate: this.state.endDate,
-            key: this.state.key + 1,
-            selected: false,
-          },
-        ],
-        companyName: " ",
-        mainTasks: " ",
-        startDate: " ",
-        endDate: "",
-        selected: false,
-        displayInputs: false,
-      },
-      () => {
-        console.log(this.state.jobExp);
-      }
-    );
-  }
-  //when the edit button is clicked for a job, populate its info in the inputs
-  editJobOnClick(name, tasks, start, end, key) {
-    this.setState({
-      displayInputs: true,
-      editing: true,
-      companyName: name,
-      mainTasks: tasks,
-      startDate: start,
-      endDate: end,
-      editKey: key,
-    });
-  }
-
-  updateJob() {
-    const jobs = [...this.state.jobExp];
-    console.log(jobs);
-    const jobToEdit = jobs.find((job) => {
-      return job.key === this.state.editKey;
-    });
-
-    jobToEdit.companyName = this.state.companyName;
-    jobToEdit.mainTasks = this.state.mainTasks;
-    jobToEdit.startDate = this.state.startDate;
-    jobToEdit.endDate = this.state.endDate;
-
-    this.setState({
-      companyName: " ",
-      mainTasks: " ",
-      startDate: " ",
-      endDate: "",
-      selected: false,
-      displayInputs: false,
-      editing: false,
-      editKey: 0,
-    });
+  createUI() {
+    return this.state.values.map((el, i) => (
+      <div key={i}>
+        <label htmlFor="companyName">Company Name</label>
+        <input
+          id="companyName"
+          type="text"
+          value={el.companyName || ""}
+          onChange={this.handleChange.bind(this, i)}
+        ></input>
+        <label htmlFor="jobTitle">Job Title</label>
+        <input
+          id="jobTitle"
+          onChange={this.handleChange.bind(this, i)}
+          value={el.jobTitle || ""}
+          type="text"
+        ></input>
+        <label htmlFor="startDate">Start Date</label>
+        <input
+          id="startDate"
+          type="date"
+          onChange={this.handleChange.bind(this, i)}
+          value={el.startDate || ""}
+        ></input>
+        <label htmlFor="endDate">End Date</label>
+        <input
+          id="endDate"
+          type="date"
+          onChange={this.handleChange.bind(this, i)}
+          value={el.endDate || ""}
+        ></input>
+        <label htmlFor="description">description</label>
+        <textarea
+          id="description"
+          type="textbox"
+          onChange={this.handleChange.bind(this, i)}
+          value={el.description || ""}
+        ></textarea>
+        <button type="button" onClick={this.removeExp.bind(this, i)}>
+          Delete
+        </button>
+        <br></br>
+      </div>
+    ));
   }
 
-  addAnotherJob() {
-    this.setState({ displayInputs: true });
+  handleChange(i, event) {
+    let values = [...this.state.values];
+    values[i][event.target.id] = event.target.value;
+    this.setState({ values });
   }
 
-  handleChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-    console.log(e.target.value);
+  addNewInputs() {
+    this.setState((prevState) => ({
+      values: [
+        ...prevState.values,
+        {
+          companyName: "",
+          jobTitle: "",
+          startDate: "",
+          endDate: "",
+          description: "",
+        },
+      ],
+    }));
+  }
+
+  removeExp(i) {
+    let values = [...this.state.values];
+    values.splice(i, 1);
+    this.setState({ values });
   }
 
   render() {
     return (
       <>
-        {
-          //display the inputs if no displayInputs is occuring
-        }
-        {this.state.displayInputs ? (
-          <form>
-            <label htmlFor="companyName"> Company Name</label>
-            <input
-              id="companyName"
-              name="companyName"
-              onChange={this.handleChange}
-              value={this.state.companyName}
-              type="text"
-            ></input>
-            <label htmlFor="mainTasks"> Job Responsibilities</label>
-            <input
-              id="mainTasks"
-              name="mainTasks"
-              onChange={this.handleChange}
-              value={this.state.mainTasks}
-              type="textarea"
-            ></input>
-            <label htmlFor="startDateJob">Start of Employment</label>
-            <input
-              id="startDateJob"
-              name="startDate"
-              onChange={this.handleChange}
-              value={this.state.startDate}
-              type="date"
-            ></input>
-            <label htmlFor="endDateJob">End of Employment</label>
-            <input
-              id="endDateJob"
-              name="endDate"
-              onChange={this.handleChange}
-              value={this.state.endDate}
-              type="date"
-            ></input>
-            {
-              //If you are editing a job, show the edit button, if it is a new job, show the new button
-            }
-            {this.state.editing ? (
-              <button type="button" onClick={this.updateJob}>
-                Update Job Experience
-              </button>
-            ) : (
-              <button type="button" onClick={this.addNewJob}>
-                Add Job Experience
-              </button>
-            )}
-          </form>
-        ) : (
-          <>
-            <br></br>
-            <button type="button" onClick={this.addAnotherJob}>
-              Add More Experience
-            </button>
-          </>
-        )}
+        <h2>Work Experience</h2>
+        {this.createUI()}
+        <button type="button" onClick={this.addNewInputs}>
+          Add Work Experience
+        </button>
+        <br></br>
       </>
     );
   }
