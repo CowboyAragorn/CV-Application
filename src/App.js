d542f8627346d097942f30bdfb4f708dd1134315
CowboyAragorn <jacobman0313@gmail.com>
2022-11-06 11:20:34 -0500

    style: add right hand svg for design

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import "./App.css";
 import EduExperience from "./components/EduExperience";
 import GenInfo from "./components/GenInfo";
 import JobExperience from "./components/JobExperience";
-
-//import JobExperienceRender from "./components/jobExperience/JobExperienceRender";
+import InterviewSVG from "./assets/InterviewSVG";
 class App extends Component {
   render() {
     return (
       <>
         <div id="pageContainer">
           <form>
-            <h1 id="pageHeader">Resume Creator</h1>
+            <h1 id="pageHeader">CV App with React</h1>
             <GenInfo />
             <EduExperience />
             <JobExperience />
@@ -20,6 +19,7 @@ class App extends Component {
               Submit CV
             </button>
           </form>
+          <InterviewSVG />
         </div>
       </>
     );
