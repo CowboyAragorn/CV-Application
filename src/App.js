2f55d437b3f9a7cc553ea4f14852d5a3d4a6c7cf
CowboyAragorn <jacobman0313@gmail.com>
2022-11-06 10:53:49 -0500

    style: round edges, introduce color pallet

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import "./App.css";
 import EduExperience from "./components/EduExperience";
 import GenInfo from "./components/GenInfo";
 import JobExperience from "./components/JobExperience";
+
 //import JobExperienceRender from "./components/jobExperience/JobExperienceRender";
 class App extends Component {
   render() {
     return (
       <>
-        <form>
-          <h1 id="pageHeader">Resume Creator</h1>
-          <GenInfo />
-          <EduExperience />
-          <JobExperience />
-          <br></br>
-          <button type="submit" id="submitBtn">
-            Submit CV
-          </button>
-        </form>
+        <div id="pageContainer">
+          <form>
+            <h1 id="pageHeader">Resume Creator</h1>
+            <GenInfo />
+            <EduExperience />
+            <JobExperience />
+            <br></br>
+            <button type="submit" id="submitBtn">
+              Submit CV
+            </button>
+          </form>
+        </div>
       </>
     );
   }
